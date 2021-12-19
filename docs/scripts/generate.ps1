b9a7339d067639b96c62236d99283766a3101a2f
devhl-labs <shawnkanyer@gmail.com>
2021-12-19 18:46:16 -0500

    updated readme, truncated app descrition, improved generate script

--- docs/scripts/generate.ps1
+++ docs/scripts/generate.ps1
@@ -3,14 +3,33 @@ $jar = Resolve-Path -Path $PSScriptRoot\..\..\..\openapi-generator\modules\opena
 $swagger = Resolve-Path -Path $PSScriptRoot\..\swagger.yml
 New-Item -Path $PSScriptRoot\..\..\generated-src -ItemType Directory -Force
 $output = Resolve-Path -Path $PSScriptRoot\..\..\generated-src
+$templates = Resolve-Path -Path $PSScriptRoot\..\templates
+
+$properties = @(
+    'projectName=Cmc'
+    'packageName=Cmc',
+    'targetFramework=net5.0'
+    'validatable=false',
+    'nullableReferenceTypes=true',
+    'hideGenerationTimestamp=false',
+    'packageVersion=1.0.0'
+) -join ","
+
+$global = @(
+    'apiDocs=false'
+    'modelDocs=false',
+    'apiTests=true',
+    'modelTests=false'
+) -join ","
 
 java -jar $jar generate `
     -g csharp-netcore `
     -i $swagger `
     -o $output `
     --library generichost `
-    --additional-properties=projectName=Cmc,packageName=Cmc,targetFramework=net5.0,validatable=false,nullableReferenceTypes=true,hideGenerationTimestamp=false `
-    --global-property=modelDocs=false,modelTests=false,apiDocs=false
+    --additional-properties $properties `
+    --global-property $global `
+    -t $templates
 
 $readme = Resolve-Path -Path $PSScriptRoot\..\..\generated-src\README.md
 $src = Resolve-Path -Path $PSScriptRoot\..\..
